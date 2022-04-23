579dfcaaa65b99ad0017c457f4a466c975e528ca
Juan Briones <JuanMBriones@outlook.com>
2022-04-23 13:09:33 -0500

    Added Singleton pattern for a global instance of Symbol table

--- semantical/symbol_tables.py
+++ semantical/symbol_tables.py
@@ -21,8 +21,22 @@ class ProcedureSymbol(Symbol):
 
         if bitmask_temp and __o.params == self.params and __o.locals == self.locals:
             return True
-        
-class SymbolTable:
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class SymbolTable(metaclass=SingletonMeta):
     def __init__(self):
         self.symbols = {}
 
--- tests/test_symbol_table.py
+++ tests/test_symbol_table.py
@@ -1,5 +1,6 @@
 import unittest
 from semantical.symbol_tables import SymbolTable, ProcedureSymbol, Symbol
+import logging
 
 class TestSymbolTable(unittest.TestCase):
     symbol_table = SymbolTable()
@@ -23,6 +24,20 @@ class TestSymbolTable(unittest.TestCase):
     
     def test_total_vars(self):
         self.assertNotEqual(self.symbol_table.get_all_variables_names(), ['a', 'foo'])
+
+    def test_singleton_instance(self):
+        new_symbol_table = SymbolTable()
+        self.assertEqual(new_symbol_table, self.symbol_table)
+    
+    def test_singleton_instance_total_vars(self):
+        new_symbol_table = SymbolTable()
+        self.assertEqual(new_symbol_table.get_all_variables_names(), self.symbol_table.get_all_variables_names())
+
+    def test_singleton_instance_total_vars_after_assigment(self):
+        new_symbol_table = SymbolTable()
+        self.symbol_table.add("a", "int", "variable", "global")
+        self.assertEqual(new_symbol_table.get_all_variables_names(), self.symbol_table.get_all_variables_names())
+    
 
 if __name__ == '__main__':
     unittest.main()
