464bffdb47e022dda24614ec40774fdcee5dbc70
Juan Briones <JuanMBriones@outlook.com>
2022-06-04 02:23:21 -0500

    Revert "Add OperationCodes for parser"
    This reverts commit e645eda1a1e23a461c142a8c40eb33a01990dbad.

--- execution/vm.py
+++ execution/vm.py
@@ -238,9 +238,9 @@ def read_file(file):
                 ip = quadruples.quadruples[ip].get_result()
             else:
                 ip += 1
-        elif cod_op == int(OperationCodes.ENDFUNC):
+        elif cod_op == 2100019:
             ip += 1
-        elif cod_op == int(OperationCodes.PRINT):
+        elif cod_op == 2100020:
             result = quadruples.quadruples[ip].get_result()
             result_value = get_value(functions_table, constants_table, result)
             print(result_value)
